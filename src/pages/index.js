ef7af399c1e275c17f75d66e623d70a3704dd48e
katya pesnya <katyapesnya@icloud.com>
2021-04-08 23:58:09 +0300

    clear constants

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,158 +9,166 @@ import PopupWithImage from '../scripts/components/PopupWithImage.js';
 import PopupWithSubmit from '../scripts/components/PopupWithSubmit.js';
 import Api from '../scripts/components/Api.js'
 import {
-  formElementEdit, formElementAdd,
-  openButtonEdit,
-  openButtonAdd, selectors, cardsList, openButtonAvatar
+    formElementEdit, formElementAdd, openButtonEdit,
+    openButtonAdd, selectors, cardsList, openButtonAvatar
 } from '../scripts/utils/constants.js';
- const options = {
-   url: 'https://mesto.nomoreparties.co/v1/cohort-21',
-   headers: {
-    Authorization: '8bc2b522-b2e5-4475-a3df-8cf1760d3928',
-     'Content-Type': 'application/json',
-     
-   }
- }
+
+const options = {
+    url: 'https://mesto.nomoreparties.co/v1/cohort-21',
+    headers: {
+        Authorization: '8bc2b522-b2e5-4475-a3df-8cf1760d3928',
+        'Content-Type': 'application/json',
+
+    }
+}
 
 const api = new Api(options)
 const userInfo = new UserInfo('.profile__title', '.profile__description', '.profile__avatar', api)
 
 //список карточек
-const cardList = new Section({  
-  renderer: (item) => { 
-    cardList.addItem(createCard(item)) 
-  } 
-}, cardsList, api) 
-   
+const cardList = new Section({
+    renderer: (item) => {
+        cardList.addItem(createCard(item))
+    }
+}, cardsList, api)
+
 // создание карточки
-function createCard(item) { 
- 
-  const card = new Card(item, '.card-template', item.owner._id, {
-    handleCardClick: () => { 
-      popupWithImage.open(item); 
-    },
-     handleDeleteIconClick: () => {
-      popupWithSubmit.open();
-    }, api
-  }); 
-  return card.generateCard(); 
-} 
-//попап подтверждения удаления карточки 
-const popupWithSubmit = new PopupWithSubmit ('.overlay_delete-card',
-{ handleFormSubmit : ({owner:{_id}}) => {
-  api.deleteCard({owner:{_id}})
-  .then(() => {
-    card._deleteCard({owner:{_id}});
-  })
-  .then(() => {
-    popupWithSubmit.close();
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+function createCard(item) {
+
+    const card = new Card(item, '.card-template', item.owner._id, {
+        handleCardClick: () => {
+            popupWithImage.open(item);
+        },
+        handleDeleteIconClick: () => {
+            popupWithSubmit.open();
+        }, api
+    });
+    return card.generateCard();
 }
-})
-popupWithSubmit.setEventListeners() 
+
+//попап подтверждения удаления карточки 
+const popupWithSubmit = new PopupWithSubmit('.overlay_delete-card',
+    {
+        handleFormSubmit: ({owner: {_id}}) => {
+            api.deleteCard({owner: {_id}})
+                .then(() => {
+                    card._deleteCard({owner: {_id}});
+                })
+                .then(() => {
+                    popupWithSubmit.close();
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    })
+popupWithSubmit.setEventListeners()
 // добавление карточки
-const popupAddForm = new PopupWithForm('.overlay_type_add', 
-  { 
-    handleFormSubmit: ({caption, url}) => {
-      popupAddForm.renderLoading(true)
-      api.createCard({caption, url })
-      .then(({name, link, owner})  =>{
-        const card = createCard({ 
-          title: name,
-          image: link,
-          owner: owner
-       });
-       cardList.prependItem(card) 
-        formAddValudator.disableSubmitButton() 
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        popupAddForm.renderLoading(false);
-        popupAddForm.close()
-      })
-  } , api
-  }) 
- 
-  popupAddForm.setEventListeners() 
+const popupAddForm = new PopupWithForm('.overlay_type_add',
+    {
+        handleFormSubmit: ({caption, url}) => {
+            popupAddForm.renderLoading(true)
+            api.createCard({caption, url})
+                .then(({name, link, owner}) => {
+                    const card = createCard({
+                        title: name,
+                        image: link,
+                        owner: owner
+                    });
+                    cardList.prependItem(card)
+                    formAddValidator.disableSubmitButton()
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => {
+                    popupAddForm.renderLoading(false);
+                    popupAddForm.close()
+                })
+        }, api
+    })
+
+popupAddForm.setEventListeners()
 
 // user info update
 api.getInfo()
-.then(({name, about, avatar}) => {
-  userInfo.setUserInfo({title: name, description: about, avatar: avatar})
-})
+    .then(({name, about, avatar}) => {
+        userInfo.setUserInfo({title: name, description: about, avatar: avatar})
+    })
 
 //edit form
-const popupEditForm = new PopupWithForm('.overlay_type_edit', 
-  { 
-    handleFormSubmit: ({ title: name,
-      description: about}) => { 
-      popupEditForm.renderLoading(true)
-      api.setInfo({ name: name,
-        about: about})
-      .then(({ name : name,
-        about : about})=>{
-        userInfo.setUserInfo({title: name, description: about})
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        popupEditForm.renderLoading(false);
-        popupEditForm.close();
-      }) 
-    }, api 
-  }); 
- 
-popupEditForm.setEventListeners(); 
+const popupEditForm = new PopupWithForm('.overlay_type_edit',
+    {
+        handleFormSubmit: ({
+                               title: name,
+                               description: about
+                           }) => {
+            popupEditForm.renderLoading(true)
+            api.setInfo({
+                name: name,
+                about: about
+            })
+                .then(({
+                           name: name,
+                           about: about
+                       }) => {
+                    userInfo.setUserInfo({title: name, description: about})
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => {
+                    popupEditForm.renderLoading(false);
+                    popupEditForm.close();
+                })
+        }, api
+    });
+
+popupEditForm.setEventListeners();
 
 //попап аватарки
-const popupAvatar = new PopupWithForm ('.overlay_avatar', 
-{
-  handleFormSubmit: ({avatar}) => {
-    popupAvatar.renderLoading(true);
-    api.setAvatar({avatar})
- 
-    .then(({avatar}) => { 
-      
-      userInfo.setUserAvatar({avatar : avatar})
+const popupAvatar = new PopupWithForm('.overlay_avatar',
+    {
+        handleFormSubmit: ({avatar}) => {
+            popupAvatar.renderLoading(true);
+            api.setAvatar({avatar})
+
+                .then(({avatar}) => {
+
+                    userInfo.setUserAvatar({avatar: avatar})
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => {
+                    popupAvatar.renderLoading(false);
+                    popupAvatar.close();
+                })
+        }, api
     })
-    .catch((err) => {
-      console.log(err);
-    })
-    .finally(() => {
-      popupAvatar.renderLoading(false);
-      popupAvatar.close();
-    })
-},api
-})
-popupAvatar.setEventListeners(); 
+popupAvatar.setEventListeners();
 
 //  попап превью картинки
-const popupWithImage = new PopupWithImage('.overlay_type_preview'); 
-popupWithImage.setEventListeners(); 
+const popupWithImage = new PopupWithImage('.overlay_type_preview');
+popupWithImage.setEventListeners();
 
 //валидация форм
-const formAddValudator = new FormValidator(selectors, formElementAdd) 
-formAddValudator.enableValidation() 
- 
-const formEditValudator = new FormValidator(selectors, formElementEdit) 
-formEditValudator.enableValidation() 
- 
- // обработчик кнопки добавления карточки
-openButtonAdd.addEventListener('click', () => { 
-  popupAddForm.open(); 
-formAddValudator.disableSubmitButton()
-}) 
+const formAddValidator = new FormValidator(selectors, formElementAdd)
+formAddValidator.enableValidation()
+
+const formEditValidator = new FormValidator(selectors, formElementEdit)
+formEditValidator.enableValidation()
+
+// обработчик кнопки добавления карточки
+openButtonAdd.addEventListener('click', () => {
+    popupAddForm.open();
+    formAddValidator.disableSubmitButton()
+})
 // обработчик кнопки редактирования пользователя
-openButtonEdit.addEventListener('click', () => { 
-  popupEditForm.open(userInfo.getUserInfo()); 
-}) 
+openButtonEdit.addEventListener('click', () => {
+    popupEditForm.open(userInfo.getUserInfo());
+})
 // обработчик кнопки редактирования аватара пользователя
 openButtonAvatar.addEventListener('click', () => {
-  popupAvatar.open()
+    popupAvatar.open()
 })
